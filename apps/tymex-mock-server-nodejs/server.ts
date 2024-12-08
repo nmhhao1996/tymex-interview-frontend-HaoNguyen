2becb471da74c0928ffbbf642ad41c36a1d42940
nmhhao1996 <nmhhao1996@gmail.com>
2024-12-08 20:00:21 +0700

    add mock server

---
+++ apps/tymex-mock-server-nodejs/server.ts
@@ -0,0 +1,14 @@
+import * as jsonServer from "json-server";
+import "dotenv/config";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT ?? 5005;
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+  console.log(`JSON Server is running at http://localhost:${port}`);
+});
