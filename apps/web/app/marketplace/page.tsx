f3518789df23cf0b8f17c5f144b37649010330e6
nmhhao1996 <nmhhao1996@gmail.com>
2024-12-13 15:07:18 +0700

    add Suspense tag

--- apps/web/app/marketplace/page.tsx
+++ apps/web/app/marketplace/page.tsx
@@ -3,31 +3,34 @@ import ProductFilter from "./components/product-filter";
 import ProductFilterDrawer from "./components/product-filter-drawer";
 import ProductList from "./components/product-list";
 import { Banner } from "./components/banner";
+import { Suspense } from "react";
 
 export default function Page(): JSX.Element {
   return (
     <>
       <Banner />
-      <div className="container mx-auto pt-14">
-        <div className="grid grid-cols-1 px-3 md:grid-cols-4 gap-8">
-          <div className="hidden md:block static">
-            <div className="sticky top-[calc(var(--header-height)+2rem)]">
-              <ProductFilter />
-            </div>
-          </div>
-          <div className="col-span-1 md:col-span-3 space-y-10">
-            <div className="flex">
-              <div className="md:hidden pr-2 mr-2 border-r border-r-input">
-                <ProductFilterDrawer />
-              </div>
-              <div className="flex-1 overflow-auto">
-                <CategoryFilter />
+      <Suspense>
+        <div className="container mx-auto pt-14">
+          <div className="grid grid-cols-1 px-3 md:grid-cols-4 gap-8">
+            <div className="hidden md:block static">
+              <div className="sticky top-[calc(var(--header-height)+2rem)]">
+                <ProductFilter />
               </div>
             </div>
-            <ProductList />
+            <div className="col-span-1 md:col-span-3 space-y-10">
+              <div className="flex">
+                <div className="md:hidden pr-2 mr-2 border-r border-r-input">
+                  <ProductFilterDrawer />
+                </div>
+                <div className="flex-1 overflow-auto">
+                  <CategoryFilter />
+                </div>
+              </div>
+              <ProductList />
+            </div>
           </div>
         </div>
-      </div>
+      </Suspense>
     </>
   );
 }
